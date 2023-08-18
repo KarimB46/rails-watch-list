ea3a7fa08fd655753b3028d3437d29f603f054ad
Karim Benzidani <padubi2000@yahoo.fr>
2023-08-18 11:26:20 +0200

    avant de commencer une nouvelle journée

--- db/migrate/20230817101533_create_films.rb
+++ db/migrate/20230817101533_create_films.rb
@@ -1,10 +1,10 @@
 class CreateFilms < ActiveRecord::Migration[7.0]
   def change
-    create_table :films do |t|
+    create_table :movies do |t|
       t.string :title
       t.string :overview
       t.string :poster_url
-      t.integer :rating
+      t.float :rating
 
       t.timestamps
     end
--- db/migrate/20230817120441_rename_films_to_movies.rb
+++
@@ -1,5 +0,0 @@
-class RenameFilmsToMovies < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :films, :movies
-  end
-end
